ebecd638dbe722d2c65fab403522fb1c293d9b83
mukmaster <helmut.roderus@hs-ansbach.de>
2019-12-22 14:25:43 +0100

    Einige Änderungen für QuizApp 2019 (v.a. in Frage 4 beim Skalieren des Bildes)

--- app/src/main/java/de/ihrprof/quizapp/Q4Activity.java
+++ app/src/main/java/de/ihrprof/quizapp/Q4Activity.java
@@ -2,6 +2,7 @@ package de.ihrprof.quizapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -9,35 +10,60 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 
-public class Q4Activity extends Activity implements OnTouchListener {
+import static java.lang.Math.round;
 
+public class Q4Activity extends Activity implements View.OnTouchListener {
+
+	private final String APP = "QuizApp";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_q4);
 		
-		ImageView img = (ImageView) findViewById(R.id.q4imageView);
-		img.setOnTouchListener(this);		
+		ImageView imageView = findViewById(R.id.q4imageView);
+		imageView.setBackgroundColor(0xff0000);
+		imageView.setOnTouchListener(this);
 	}
 	
 	public boolean onTouch(View view, MotionEvent evt){
-		// Zun�chst besorgen wir uns die Breite und H�he des angeklickten Bilds (in Pixeln)
-		float imgWidth = view.getWidth();
+
+		// debugImageView(view, evt);
+		// Zun�chst besorgen wir uns die Breite und H�he des angeklickten Views (in Pixeln)
+		float viewWidth = view.getWidth();
+		float viewHeight = view.getHeight();
+		float imgWidth = view.getMeasuredWidth();
 		float imgHeight = view.getHeight();
 		// Und dann die Koordinaten des Touchevents (ebenfalls Pixel)
-		float x = evt.getX();
+		float x = evt. getX();
 		float y = evt.getY();
-		// Die Koordinaten werden von absolut in relativ umgerechnet
-		x = x/imgWidth;
-		y = y/imgHeight;
-		// Dank Photoshop wissen wir, dass der gesuchte Bildbereich bei x>=85% und 4%<=y<=13% liegt
-		boolean treffer = (x>=0.85) && (y>=0.04) && (y<=0.13); 
+
+		// Den Koordinatenbereich für einen Treffen haben wir vorab mit Hilfe der Methode debugImageView herausgefunden
+		boolean treffer = (x>=585 && x<=656 && y>=45 && y<=120);
 		if (treffer)
 			Helper.feedback(true,1,this);
 		else
 			Helper.feedback(false,0,this);
-		return true;
+		return false;
+	}
+
+	private void debugImageView(View v, MotionEvent evt){
+		// Die folgende Methode hilft, die Koordinaten des Bildes zu analysieren
+		// Sie muss in onTouch aktiviert werden (Kommentar weg) und sie protokolliert
+		// Ihre Ergebnisse mit Hilfe von Log.i() (siehe Logcat)
+		ImageView iv = (ImageView) v;
+		float viewWidth = round(iv.getWidth());
+		float viewHeight = round(iv.getHeight());
+		float imgWidth = round(iv.getDrawable().getIntrinsicWidth());
+		float imgHeight = round(iv.getDrawable().getIntrinsicHeight());
+		float x = round(evt.getX());
+		float y = round(evt.getY());
+		boolean treffer = (x>=585 && x<=656 && y>=45 && y<=120);
+
+		Log.i(APP, "ImageView.getWidth/-Height:" + viewWidth + ", " + viewHeight);
+		Log.i(APP, "Drawable.getMinimumWidth/-Height:" + imgWidth + ", " + imgHeight);
+		Log.i(APP, "TouchEvent.getX-/Y:" + x + ", " + y);
+		Log.i(APP, "Treffer: " + treffer);
 	}
 
 	@Override
