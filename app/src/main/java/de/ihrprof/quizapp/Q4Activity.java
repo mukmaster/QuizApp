92af8bd8db5ef9d8cf4fd53b16e41ca6228a2bfe
mukmaster <helmut.roderus@hs-ansbach.de>
2019-12-22 18:27:50 +0100

    Einige Änderungen für QuizApp 2019 im Zusammenhang mit Frage 4 (Skalieren des Bildes)

--- app/src/main/java/de/ihrprof/quizapp/Q4Activity.java
+++ app/src/main/java/de/ihrprof/quizapp/Q4Activity.java
@@ -27,24 +27,28 @@ public class Q4Activity extends Activity implements View.OnTouchListener {
     }
 
     public boolean onTouch(View view, MotionEvent evt) {
-
+        // Für Debugzwecke den folgenden Methodenaufruf auskommentieren:
         // debugImageView(view, evt);
+
         // Zunächst besorgen wir uns die Breite und Höhe des angeklickten Views (in Pixeln)
         float viewWidth = view.getWidth();
         float viewHeight = view.getHeight();
-        float imgWidth = view.getMeasuredWidth();
-        float imgHeight = view.getHeight();
+
         // Und dann die Koordinaten des Touchevents (ebenfalls Pixel)
         float x = evt.getX();
         float y = evt.getY();
 
-		/*
-		Den Koordinatenbereich für einen Treffen haben wir vorab mit Hilfe der Methode debugImageView herausgefunden.
-		Bitte beachten: Bilder können von Android skaliert werden, damit sie in den ImageView passen. In unserem
-		Beispiel wurde im Layout von activity_q4.xml festgelegt, dass das Bild in den ImageView eingepasst wird (scaleType="fitXY")
-		und außerdem mit adjustViewBounds = "true" verhindert, dass von Android noch Padding-Space um das Bild gelegt wird.
+        // Die Touch-Koordinaten umrechnen in relative Koordinaten (die absoluten Koordinaten hängen von der Displayauflösung ab):
+        float xRel = x/viewWidth;
+        float yRel = y/viewHeight;
+
+		/*  Den Koordinatenbereich für einen Treffen haben wir vorab mit Hilfe der Methode debugImageView herausgefunden.
+		    Bitte beachten: Bilder können von Android skaliert werden, damit sie in den ImageView passen. In unserem
+		    Beispiel wurde im Layout von activity_q4.xml festgelegt, dass das Bild in den ImageView eingepasst wird (scaleType="fitXY")
+		    und außerdem mit adjustViewBounds = "true" verhindert, dass von Android noch Padding-Space um das Bild gelegt wird.
 		*/
-        boolean treffer = (x >= 585 && x <= 656 && y >= 45 && y <= 120);
+
+        boolean treffer = (xRel>=0.85 && yRel>=0.04 && yRel<=0.13);
         if (treffer)
             Helper.feedback(true, 1, this);
         else
@@ -53,9 +57,9 @@ public class Q4Activity extends Activity implements View.OnTouchListener {
     }
 
     private void debugImageView(View v, MotionEvent evt) {
-        // Die folgende Methode hilft, die Koordinaten des Bildes zu analysieren
-        // Sie muss in onTouch aktiviert werden (Kommentar weg) und sie protokolliert
-        // Ihre Ergebnisse mit Hilfe von Log.i() (siehe Logcat)
+        // Die folgende Methode hat mir geholfen, die Größe und die Skalierung des Bildes zu analysieren
+        // Sie muss in onTouch aktiviert werden (Kommentar weg). Sie protokolliert mit Hilfe von Log.i(),
+        // also die Ergebnisse im Logcat nachsehen!
         ImageView iv = (ImageView) v;
         float viewWidth = round(iv.getWidth());
         float viewHeight = round(iv.getHeight());
@@ -63,11 +67,16 @@ public class Q4Activity extends Activity implements View.OnTouchListener {
         float imgHeight = round(iv.getDrawable().getIntrinsicHeight());
         float x = round(evt.getX());
         float y = round(evt.getY());
-        boolean treffer = (x >= 585 && x <= 656 && y >= 45 && y <= 120);
+        float xRel = x/viewWidth;
+        float yRel = y/viewHeight;
+
+        boolean treffer = (xRel>=0.85 && yRel>=0.04 && yRel<=0.13);
+        // boolean treffer = (x >= 585 && x <= 656 && y >= 45 && y <= 120);
 
         Log.i(APP, "ImageView.getWidth/-Height:" + viewWidth + ", " + viewHeight);
         Log.i(APP, "Drawable.getMinimumWidth/-Height:" + imgWidth + ", " + imgHeight);
         Log.i(APP, "TouchEvent.getX-/Y:" + x + ", " + y);
+        Log.i(APP, "Relativ: " + xRel + ", " + yRel);
         Log.i(APP, "Treffer: " + treffer);
     }
 
